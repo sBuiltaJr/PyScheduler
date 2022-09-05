579f214d983ed727d8167a166ef238de3aae9f8d
Random <invalid@GitIsDumbSometimes.com>
2022-09-04 17:07:51 -0700

    Capturing interring updates and basic script to make the necessary user and databases.

---
+++ create_db_linux.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+PSB_host=localhost
+PSB_name=PSB
+PSB_pass=password
+#First create the user if they don't already exist.  Must be done with root,
+#meaning sudo.  This also covers people who only installed mariadb and didn't
+#expressly make the bot a user account in the database (which it needs).
+sudo mariadb -u root -e "CREATE USER IF NOT EXISTS $PSB_name@$PSB_host IDENTIFIED BY '$PSD_pass';"
+#As of at least Mariadb 10, the user can be made before the DB (even though
+#the syntax implies the reverse).  Futher script updates will probably check
+#for sudo properly before executing these first 4 commands.
+sudo mariadb -u root -e "GRANT ALL PRIVILEGES ON PSBEvents.* TO $PSB_name@$PSB_host;"
+sudo mariadb -u root -e "GRANT ALL PRIVILEGES ON PSBGuilds.* TO $PSB_name@$PSB_host;"
+sudo mariadb -u root -e "GRANT ALL PRIVILEGES ON PSBCalendars.* TO $PSB_name@$PSB_host;"
+
+#Everything else can be accomplished by the bot, and should for verification.
+mariadb -u$PSB_name -p$PSB_pass -h$PSB_host -e "CREATE DATABASE IF NOT EXISTS PSBEvents;"
+mariadb -u$PSB_name -p$PSB_pass -h$PSB_host -e "CREATE DATABASE IF NOT EXISTS PSBGuilds;"
+mariadb -u$PSB_name -p$PSB_pass -h$PSB_host -e "CREATE DATABASE IF NOT EXISTS PSBCalendars;"
--- database/mariadbIfc.py
+++ database/mariadbIfc.py
@@ -28,16 +28,17 @@ class mariadbIfc:
 		"""
 		
 		self.args = {}
+		self.cmds = {}
 		
 		#The config file lcoation is nonconfigurable, for now.
 		try:
 			json_file = open('mariadb_cfg.json')
 			self.args = json.load(json_file)
 		#Sure, it's more pythonic to use with and only catch limited exceptions, 
-		#but making a case here for every possible exception type is dum.
+		#but making a case here for every possible exception type is dumb.
 		except Exception as e:
-			print(f"Error opening the mariadb config file: {e=}")
-			exit(1)
+			print(f"Error opening the mariadb config files: {e=}")
+			sys.exit(1)
 		
 		os.makedirs(self.args['log_dir'], \
 						mode=int(self.args['log_file_mode'], 8),\
@@ -49,7 +50,16 @@ class mariadbIfc:
 									self.args['log_dir'] + os.sep + 'db_ifc_log.log'))
 		self.db_log.propagate = False
 		self.db_log.info(f"Mariadb Logger logging to {self.args['log_dir']}")
-
+		
+		#We can now at least use the logger for errors.
+		try:
+			json_file = open('mariadb_commands.json')
+			self.cmds = json.load(json_file)
+		#Sure, it's more pythonic to use with and only catch limited exceptions, 
+		#but making a case here for every possible exception type is dumb.
+		except Exception as e:
+			self.db_log.error(f"Unable to get MariaDB commands: {e=}")
+			sys.exit(1)
 
 	def validateInstall(self, create : bool):
 		"""Validates all the database components are accessable and usable by the
@@ -61,8 +71,34 @@ class mariadbIfc:
 			
 			Output: bool - True if install is valid and usable.
 		"""
-		all_ok = False
+		#This start true so the acces attempt can set it false to signal the
+		#create option
+		all_ok = True
 		
+		try:
+			self.con = mariadb.connect(host=self.args['host'], \
+												port=int(self.args['port']), \
+												user=self.args['user_name'], \
+												password=self.args['password'])
+		except mariadb.Error as err:
+			self.db_log.error(f"Error connecting to mariadb: {err=}")
+			all_ok = False
+			
+			if not create :
+				return all_ok
+		
+		if not all_ok:
+			#At best we can attempt to try the default user/password connection,
+			#and exit if it fails.  From tehr eonly the user can fix the problem.
+			try:
+				self.con = mariadb.connect(host="127.0.0.1", \
+													port=3306, \
+													user="root", \
+													password="")
+			except mariadb.Error as err:
+				self.db_log.error("Unable to log in with root using the default mariadb settings, you must make an account for me first or allow root access. Or is the service off?")
+				self.db_log.error(f"Error: {err=}") 
+										
 		return all_ok
 
 if __name__ == '__main__':
