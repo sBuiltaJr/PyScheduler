2bc9194a23f71c6764bb81e72b083fe8acc0aa5f
sBuiltaJr <106572743+sBuiltaJr@users.noreply.github.com>
2022-09-09 21:10:40 -0700

    Finally found a solution to the annoying psudo-positional argument problem, see config for cases.  Still need to finish others.

--- utilities/command_parser.py
+++ utilities/command_parser.py
@@ -74,11 +74,13 @@ class CommandParser:
         self.cfgSub = self.cfgAp.add_subparsers()
         self.cfgSubMsg = self.cfgSub.add_parser('msg', \
                                                 help=self.txt['cfg_msg_help'])
-#        self.cfgSubMsg.add_argument('msg_txt', type=str, \
-#                                     help=self.txt['cfg_msg_help'])
+        self.cfgSubMsg.add_argument('msg_txt', type=str, \
+                                     help=self.txt['cfg_msg_help'])
         self.cfgSubRmd = self.cfgSub.add_parser('remind',\
                                                 help=self.txt['cfg_rmd_help'])
-        self.cfgSubRmd.add_argument('-remove', type=str)
+        self.cfgSubRmd.add_argument('remove', type=bool, nargs='?', \
+                                    default=False)
+        self.cfgSubRmd.add_argument('msg', type=str, nargs='?', default='')
         self.cfgSubChn = self.cfgSub.add_parser('chan', \
                                                 help=self.txt['cfg_chn_help'])
         self.cfgSubRms = self.cfgSub.add_parser('remind-msg', \
@@ -112,14 +114,16 @@ class CommandParser:
            
            Output: list - A list of the arguments (null if invalid).
         """
-        cmd_str    = ''
-        ret        = []
-        usr_string = []
+        cmd_str = ''
+        ret     = []
+        usr_str = []
+
+        self.cmd_log.debug(f"Given user command: {cmd}")
         
         try:
-            usr_string = str(cmd).split(sep=' ',maxsplit=1)
+            usr_str = str(cmd).split(sep=' ',maxsplit=1)
             #The slash has not yet been stripped.
-            function   = self.funcs[(usr_string[0])[1:].lower()]
+            function   = self.funcs[(usr_str[0])[1:].lower()]
 
         except KeyError as err:
             self.cmd_log.error(f"Invalid command prefix in string: {cmd}")
@@ -128,26 +132,30 @@ class CommandParser:
         #This is necessary to avoid split issues with commands as the original
         #bot allowed for unrestricted message format when quoted.  This makes
         #a split on the other, space-separated, command elements hard.
-        #Fortuantely the quotes are requored to by the last element, allowing
+        #Fortuantely the quotes are required to by the last element, allowing
         #us to scan for them and manually insert the string as an argument at
         #the end of an arg list.
-        if args.endswith('"'):
-                #User formmated commands can be of any length or pattern,
-                #but shouldn't contain quotes, so we just need to parse
-                #backwards to where another quote can be found to complete the
-                #pair.
-                cmd_index = args.rfind('"', 0,-1)
+        print(f"usr_str: {usr_str}")
+        if usr_str[1].endswith('"'):
+            #User formmated commands can be of any length or pattern, but
+            #shouldn't contain quotes, so we just need to parse backwards to
+            #where another quote can be found to complete the pair.
+            cmd_index = usr_str[1].rfind('"', 0,-1)
 
-                #Technically the else is also an error but the parser can
-                #handle it.
-                if cmd_index > 0:
-                    cmd_str = cmd[cmd_index:]
-                    args    = cmd[:cmd_index]
-                    print(f"split: {cmd_str}, {cmd_index}, {args}")
-        usr_args = args.split(' ')
+            #Technically the else is an error but the parser can handle it.
+            if cmd_index > -1:
+                cmd_str    = usr_str[1][cmd_index:]
+                usr_str[1] = \
+                        usr_str[1][:((cmd_index - 1) if cmd_index > 0 else 0)]
+                self.cmd_log.debug(f"User args had string arg: {cmd_str}")
+                print(f"split: {usr_str}, {cmd_index}, {cmd_str}")
+        usr_args = usr_str[1].split(' ')
         usr_args.append(cmd_str)
+        #Todo: this is really just a hack.
+        parse_args = list(filter(None, usr_args))
+        print(f"args: {parse_args}")
 
-        ret = function(usr_args)
+        ret = function(parse_args)
 
         return ret
 
@@ -166,14 +174,14 @@ class CommandParser:
         ret = []
 
         try:
-            self.cmd_log.debug(f"Annc args are: {args.split(' ')}")
+            self.cmd_log.debug(f"Annc args are: {args}")
             ret = self.annAp.parse_args(args);
         except Exception as err:
             self.cmd_log.error(f"Error in AnnParse: {err=}")
 
         return ret
 
-    def ParseConfig(self, args : str) -> list:
+    def ParseConfig(self, args : list) -> list:
         """Parses the config command.  Config modifies schedules and requires
            at least 2 arguments; one for the channel and one for the config.
 
@@ -184,22 +192,8 @@ class CommandParser:
         """
         ret = []
         try:
-            if args.endswith('"'):
-                #User formmated commands can be of any length or pattern,
-                #but shouldn't contain quotes, so we just need to parse
-                #backwards to where another quote can be found to complete the
-                #pair.
-                cmd_index = args.rfind('"', 0,-1)
-
-                #Technically the else is also an error but the parser can
-                #handle it.
-                if cmd_index > 0:
-                    cmd_str = args[cmd_index:]
-                    args = args[:cmd_index]
-                    print(f"split: {cmd_str}, {cmd_index}, {args}")
-
-            self.cmd_log.debug(f"cfg args are: {args.split(' ')}")
-            ret = self.cfgAp.parse_args(args.split(' '));
+            self.cmd_log.debug(f"cfg args are: {args}")
+            ret = self.cfgAp.parse_args(args);
             print(f"ret: {ret}")
         except Exception as err:
             self.cmd_log.error(f"Error in CfgParse: {err=}")
@@ -207,7 +201,7 @@ class CommandParser:
 
         return ret
 
-    def ParseCreate(self, args : str) -> list:
+    def ParseCreate(self, args : list) -> list:
         """Parses the create command.  Create requires at least 3 arguments and
            can have many for specifying comments, roles, and more.
 
@@ -220,7 +214,7 @@ class CommandParser:
 
         return ret
 
-    def ParseDelete(self, args : str) -> list:
+    def ParseDelete(self, args : list) -> list:
         """Parses the delete command.  Delete may specify an optional command
            for deleteing events, channels, or all schedules in the guild.
 
@@ -233,7 +227,7 @@ class CommandParser:
 
         return ret
 
-    def ParseDiagnose(self, args : str) -> list:
+    def ParseDiagnose(self, args : list) -> list:
         """Parses the diagnose command.  This is a argument-less command that
            performs basic setup checks for the bot (such as guild/channel
            access or definition).
@@ -247,7 +241,7 @@ class CommandParser:
 
         return ret
 
-    def ParseEdit(self, args : str) -> list:
+    def ParseEdit(self, args : list) -> list:
         """Parses the edit command.  Edit may specify a series of arguments,
            up to the total elements in an event.  At least 2 are required.
 
@@ -260,7 +254,7 @@ class CommandParser:
 
         return ret
 
-    def ParseEvents(self, args : str) -> list:
+    def ParseEvents(self, args : list) -> list:
         """Parses the events command.  Events has one optional argument, used
            to filter events for a specific channel instead of 'all'.
 
@@ -273,7 +267,7 @@ class CommandParser:
 
         return ret
 
-    def ParseGuild(self, args : str) -> list:
+    def ParseGuild(self, args : list) -> list:
         """Parses the guild command.  Guild requires at least 2 arguments, one
            for the type of information modified, and one for its config.
 
@@ -286,7 +280,7 @@ class CommandParser:
 
         return ret
 
-    def ParseInit(self, args : str) -> list:
+    def ParseInit(self, args : list) -> list:
         """Parses the init command.  Init may specify an optional command
            channel, provided as a channel ID.
 
@@ -300,7 +294,7 @@ class CommandParser:
 
         return ret
 
-    def ParseList(self, args : str) -> list:
+    def ParseList(self, args : list) -> list:
         """Parses the list command.  List requires one argument and up to 3,
            1 is the event ID to lsit and the other 2 are dispaly and filters.
 
@@ -313,7 +307,7 @@ class CommandParser:
 
         return ret
 
-    def ParseManage(self, args : str) -> list:
+    def ParseManage(self, args : list) -> list:
         """Parses the manage command.  Oauth has 4 required arguments: 1 for
            the event ID, 1 for the command type (add/remove), 1 for the event
            group, and 1 for the user ID.
@@ -327,7 +321,7 @@ class CommandParser:
 
         return ret
 
-    def ParseOauth(self, args : str) -> list:
+    def ParseOauth(self, args : list) -> list:
         """Parses the oauth command.  Oauth has 1 required argument, the oauth
            token necessary to access the schedule's associated Google Calendar.
 
@@ -340,7 +334,7 @@ class CommandParser:
 
         return ret
 
-    def ParsePurge(self, args : str) -> list:
+    def ParsePurge(self, args : list) -> list:
         """Parses the purge command.  Purge requries one argument, the channel
            from which to delete (up to 100) bot messages from per invocation.
 
@@ -353,7 +347,7 @@ class CommandParser:
 
         return ret
 
-    def ParseSchedules(self, args : str) -> list:
+    def ParseSchedules(self, args : list) -> list:
         """Parses the schedules command.  This is a argument-less command that
            simply lists all schedules assigned to the guild.
 
@@ -366,7 +360,7 @@ class CommandParser:
 
         return ret
 
-    def ParseSkip(self, args : str) -> list:
+    def ParseSkip(self, args : list) -> list:
         """Parses the skip command.  Skip has 1 required argument; which event
            ID to skip (and schedule to the next entry).
 
@@ -379,7 +373,7 @@ class CommandParser:
 
         return ret
 
-    def ParseSort(self, args : str) -> list:
+    def ParseSort(self, args : list) -> list:
         """Parses the sort command.  Sort has 1 required argument and 1
            optional; 1 for which  channel to sorte events in (up to 15), and 
            1 for the order (descending by date is otherwise assumed).
@@ -393,7 +387,7 @@ class CommandParser:
 
         return ret
 
-    def ParseSync(self, args : str) -> list:
+    def ParseSync(self, args : list) -> list:
         """Parses the sync command.  Sync has 1 required argument and 2
            optional, 1 for which channel to sync and 2 for how to sync the 
            channel (import/export form Google, and which calendar).
@@ -407,7 +401,7 @@ class CommandParser:
 
         return ret
 
-    def ParseTest(self, args : str) -> list:
+    def ParseTest(self, args : list) -> list:
         """Parses the test command.  Test has 1 required argument and 1
            optional, 1 for the test message to send, and 1 for what type of 
            test message (such as start, remind, or end).
@@ -421,7 +415,7 @@ class CommandParser:
 
         return ret
 
-    def ParseZones(self, args : str) -> list:
+    def ParseZones(self, args : list) -> list:
         """Parses the zones command.  Zones has 1 required argument, used to
            filter for which calendars fall in the specified zone.
 
@@ -442,17 +436,17 @@ if __name__ == '__main__':
                         filemode='a', \
                         level=log.INFO)
     cp = CommandParser()
-#    ret = cp.SortCommand("/init This is a Test!  This is only a test.")
+#    ret = cp.SortCommand('/init "This is a Test!  This is only a test."')
 #    print(f"ret = {ret}")
 
-#    ret = cp.SortCommand("/announcements J09DlA add 4893839 start-1h 'stuff'")
-#    ret = cp.SortCommand("/announcements J09DlA remove 1")
-#    ret = cp.SortCommand("/announcements J09DlA")
+#    ret = cp.SortCommand('/announcements J09DlA add 4893839 start-1h "stuff"')
+#    ret = cp.SortCommand('/announcements J09DlA remove 1')
+#    ret = cp.SortCommand('/announcements J09DlA')
 
 #    ret = cp.SortCommand('/config 474738')
     ret = cp.SortCommand('/config 474738 msg "@here The event %t %a. %f"')
     ret = cp.SortCommand('/config 474738 remind "10, 20, 30 min"')
-    ret = cp.SortCommand('/config 474738 remind -remove "20 min"')
+    ret = cp.SortCommand('/config 474738 remind remove "20 min"')
 #    ret = cp.SortCommand('/config 474738 end-remind "10 min"')
 #    ret = cp.SortCommand('/config 474738 chan "general"')
 #    ret = cp.SortCommand('/config 474738 remind-msg "reset"')
